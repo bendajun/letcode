94d6979c74a095ad7417bf129c6187fed9e4147b
杜大鹏-hz21107457 <dudapeng@huize.com>
2022-05-19 10:06:23 +0800

    feat: 新增11.12

---
+++ 11.二进制求和.js
@@ -0,0 +1,45 @@
+/**
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * 
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ */
+
+/**
+ * 时间复杂度：O(n)，其中 n 是字符串的a or b长度。
+ * 空间复杂度：O(1)。
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  const res = []
+  let c = 0
+  let [i, j] = [a.length - 1, b.length -1]
+  while (i >= 0 || j >= 0 || c) {
+    const aa = Number(a[i]) ? Number(a[i]) : 0
+    const bb = Number(b[j]) ? Number(b[j]) : 0
+    let sum = aa + bb + c
+    if (sum === 3) {
+        sum = 1
+        c = 1
+    } else if (sum === 2) {
+        sum = 0
+        c = 1
+    } else {
+        c = 0
+    }
+    res.unshift(sum)
+    i--
+    j--
+  }
+  return res.join('')
+};
+
+console.log(addBinary('11', '1'))
+
+console.log(addBinary('1010', '1011'))
---
+++ 12.x 的平方根.js
@@ -0,0 +1,37 @@
+/**
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+  由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+  注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+
+  输入：x = 4
+  输出：2
+
+  输入：x = 8
+  输出：2
+  解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ */
+
+/**
+ * 时间复杂度：O(logx)，即为二分查找需要的次数
+ * 空间复杂度：O(1)。
+ * @param {*} x 
+ * @returns 
+ */
+var mySqrt = function (x) {
+  if (x == 0 || x == 1) return x
+    
+  for (let i = 1; i <= x; i++) {
+    if (i * i === x) return i
+    if (i * i > x) return i - 1
+  }
+};
+
+console.log(mySqrt(2))
+
+console.log(mySqrt(4))
+
+console.log(mySqrt(8))
+
+console.log(mySqrt(9))
+
+console.log(mySqrt(24))
