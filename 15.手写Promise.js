be0d0a1bc7ba6a2f98ba55faae4e6da5fc2806b1
hz21107457 <dudapeng@huize.com>
2024-04-25 09:52:52 +0800

    feat: 提交

--- 15.手写Promise.js
+++ 15.手写Promise.js
@@ -3,101 +3,131 @@ const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 
 class MyPromise {
-
-  constructor(fn) {
+  constructor (fn) {
     if (typeof fn !== 'function') {
-      throw Error('an argument is not a function')
+      throw new Error('an argument is not a function')
+    }
+    this.state = PENDING
+    this.value = undefined
+    this.resolveCallbacks = []
+    this.rejectCallbacks = []
+
+    const resolve = (value) => {
+      if (this.state !== PENDING) return
+      this.state = FULFILLED
+      this.value = value
+      
+      this.resolveCallbacks.forEach(fn => fn())
     }
 
-    this._state = PENDING
-    this._value = undefined
-    this._handles = []
+    const reject = (reason) => {
+      if (this.state !== PENDING) return
+      this.state = REJECTED
+      this.value = reason
+      this.rejectCallbacks.forEach(fn => fn())
+    }
 
     try {
-      fn(this._resolve.bind(this), this._reject.bind(this))
+      fn(resolve, reject)
     } catch (e) {
-      this._reject(e)
+      reject(e)
     }
   }
 
-  _resolve(val) {
-    this._changeState(FULFILLED, val)
-  }
-
-  _reject(val) {
-    this._changeState(REJECTED, val)
-  }
-
-  _changeState(state, value) {
-    if (this._state !== PENDING) return
-    this._state = state
-    this._value = value
-  }
-
-  _runHandles() {
-    if (this._state === PENDING) return
-    while (this._handles[0]) {
-      this._runOneHandles(this._handles[0])
-      this._handles.shift(); // 每次执行完都要弹出去
-    }
-  }
-
-  _runOneHandles({ executor, state, resolve, reject }) {
-    if (state !== this._state) return;
-    if (typeof executor !== "function") {
-      state === FULFILLED ? resolve(this._value) : reject(this._value);
-      return;
-    }
-    try {
-      const resp = executor(this._value);
-      if (isPromise(resp)) {
-        resp.then(resolve, reject);
-      } else {
-        resolve(resp);
+  then (onFulfilled, onRejected) {
+    return new Promise((resolve, reject) => {
+      if (this.state === FULFILLED) {
+        const val = onFulfilled(this.value)
+        if (isPromise(val)) {
+          val.then(y => resolve(y), r => reject(r))
+        } else {
+          resolve(val)
+        }
       }
-    } catch (reason) {
-      reject(reason);
-    }
-  }
-
-  /**
-   * 向任务队列里面添加一项
-   * @param {Function} executor 需要执行的函数
-   * @param {String} state 当前这个函数是在什么状态下执行
-   * @param {Function} resolve 设置当前promise为成功
-   * @param {Function} reject 设置当前Promise为失败
-   */
-  _addOneHandles(executor, state, resolve, reject) {
-    this._handles.push({
-      executor,
-      state,
-      resolve,
-      reject,
-    });
-  }
-
-  // 成功之后调用 onFulfilled， 失败之后调用 onRejected
-  then(onFulfilled, onRejected) {
-
-    return new MyPromise((resolve, reject) => {
-      this._addOneHandles(onFulfilled, FULFILLED, resolve, reject);
-      this._addOneHandles(onRejected, REJECTED, resolve, reject);
-      this._runHandles()
-    });
+  
+      if (this.state === REJECTED) {
+        const reason = onRejected(this.value)
+        if (isPromise(reason)) {
+          val.then(y => resolve(y), r => reject(r))
+        } else {
+          reject(reason)
+        }
+      }
+  
+      if (this.state === PENDING) {
+        this.resolveCallbacks.push(() => {
+          const val = onFulfilled(this.value)
+          if (isPromise(val)) {
+            val.then(y => resolve(y), r => reject(r))
+          } else {
+            resolve(val)
+          }
+        })
+        this.rejectCallbacks.push(() => {
+          const reason = onRejected(this.value)
+          if (isPromise(reason)) {
+            val.then(y => resolve(y), r => reject(r))
+          } else {
+            reject(reason)
+          }
+        })
+      }
+    })
   }
 }
 
+
 const p1 = new MyPromise((resolve, reject) => {
-  resolve(333)
+  setTimeout(() => {
+    resolve('this is data')
+  }, 1000)
 })
 
+/* p1.then(data => {
+  console.log(data, 1)
+}, err => {
+  console.log(err, 9)
+})
 
-console.log(p1, 'p1')
+p1.then(data => {
+  console.log(data, 2)
+}, err => {
+  console.log(err, 9)
+})
+
+p1.then(data => {
+  console.log(data, 3)
+}, err => {
+  console.log(err, 9)
+}) */
 
 
-
-
-
+p1.then(data => {
+  console.log(data, 1)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('then1 return')
+    }, 1000)
+  })
+}, err => {
+  console.log(err, 'err1')
+})
+.then(data => {
+  console.log(data, 2)
+  return 'then2 return'
+}, err => {
+  console.log(err, 'err2')
+})
+.then(data => {
+  console.log(data, 3)
+}, err => {
+  console.log(err, 'err3')
+})
+.then(data => {
+  console.log(data, 4)
+}, err => {
+  console.log(err, 'err4')
+})
 
 /**
  * 判断函数是否为promise
